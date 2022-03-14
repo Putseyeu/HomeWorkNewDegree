6a2f0be567a3947ea255e688da1a46402ca534ed
Putseyeu <putseyeu.a.d@gmail.com>
2022-03-14 23:10:57 +0100

    step1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkNewDegree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int minNumber = 1;
+            int maxNumber = 100;
+            int numberFromTask = 2;
+            int randomNumber = random.Next(minNumber, maxNumber);
+            double numberСomputed = 0;
+            int degree = 0;
+
+            while(numberСomputed < randomNumber)
+            {
+                degree++;
+                numberСomputed = Math.Pow(numberFromTask, degree);
+            }
+            Console.WriteLine($"Рандомное число {randomNumber}, число {numberFromTask} в степени {degree} равно {numberСomputed}");
+        }
+    }
+}
